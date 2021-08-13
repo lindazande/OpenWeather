284826987f4dc985ef0f79d9167bbe93a5559c91
Linda Zande <linda.zande@accenture.com>
2021-08-13 19:47:57 +0300

    Initial Commit

---
+++ OpenWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenWeather
+//
+//  Created by linda.zande on 13/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
